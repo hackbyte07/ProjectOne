04dc1ed83d6ba090e99b0388123d6d020a3766cd
Aayush Rana <aayushranahardi@gmail.com>
2023-06-15 17:29:40 +0530

    upgraded react native version to 0.71.11

--- app/screens/AboutScreen.tsx
+++ app/screens/AboutScreen.tsx
@@ -1,18 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { NativeStackScreenProps } from '@react-navigation/native-stack'
-import { RootScreens } from '../navigation/RootNavigation'
-import WebView from 'react-native-webview'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootScreens} from '../navigation/RootNavigation';
+import WebView from 'react-native-webview';
 
-const AboutScreen = ({navigation} : NativeStackScreenProps<RootScreens>) => {
+const AboutScreen = ({navigation}: NativeStackScreenProps<RootScreens>) => {
   return (
-   <SafeAreaView>
-    <WebView source={{uri: 'https://www.google.com'}}/>
-   </SafeAreaView>
-  )
-}
+    <SafeAreaView>
+      <WebView source={{uri: 'https://www.google.com/'}} style={styles.webView} />
+    </SafeAreaView>
+  );
+};
 
-export default AboutScreen
+export default AboutScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  webView: {
+    flex: 1,
+  }
+});
